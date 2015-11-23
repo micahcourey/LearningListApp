714a1df458080785ef26daef020f346211ff1df1
micahcourey <micahjcourey@gmail.com>
2015-11-22 19:58:07 -0800

    Add main controller to app.js and add to index.html

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,15 @@
-angular.module("learningApp", []);
+angular.module("learningApp", [])
+
+.controller('mainCtrl', function($scope) {
+  $scope.learningNgChange = function() {
+    console.log("An input Changed")
+  };
+  $scope.learnings = [
+    {"name": "JavaScript"},
+    {"name": "AngularJS"},
+    {"name": "Node.js"},
+    {"name": "Sass"},
+    {"name": "MongoDB"},
+  ]
+
+});
