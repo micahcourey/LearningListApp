bf1d8533fab4914defd71e1113e9cd20871cf203
micahcourey <micahjcourey@gmail.com>
2015-11-23 17:42:56 -0800

    Move learing list data to learnings.json file and create a service to load the data to the controller

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,26 @@
 angular.module("learningApp", [])
 
-.controller('mainCtrl', function($scope) {
+.controller('mainCtrl', function($scope, dataService) {
+  $scope.helloConsole = dataService.helloConsole;
+
   $scope.learningNgChange = function() {
-    console.log("An input Changed")
+    console.log("An input Changed");
   };
-  $scope.learnings = [
-    {"name": "JavaScript"},
-    {"name": "AngularJS"},
-    {"name": "Node.js"},
-    {"name": "Sass"},
-    {"name": "MongoDB"},
-  ]
+
+  dataService.getLearnings(function(response) {
+    console.log(response.data);
+    $scope.learnings = response.data;
+  })
+
+})
+
+.service('dataService', function($http) {
+  this.helloConsole = function() {
+    console.log('This is the hello console service');
+  };
+
+  this.getLearnings = function(callback){
+    $http.get('mock/learnings.json').then(callback)
+  }
 
 });
